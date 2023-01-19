7df9151a16690a2b3245bc53a90aa4952cab485e
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2023-01-19 09:52:10 +0200

    in procces

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+const filters = 'fields=name,capital,population,flags,languages';
+
+function fetchCountries(name) {
+  const url = `${BASE_URL}/name/${name}?${filters}`;
+
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+export { fetchCountries };
